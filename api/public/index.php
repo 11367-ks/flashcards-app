93b3a728464fc2466ae178df30bf09e6bfacc5f9
Karol <11367@kasprzak.edu.pl>
2024-09-26 13:54:42 +0200

    API file structure update

--- api/public/index.php
+++ api/public/index.php
@@ -1,17 +1,14 @@
 <?php
 header('Content-Type: application/JSON');
 include '../inc/config.php';
-include '../model/Database.php';
-include '../model/FlashcardsModel.php';
-
-$db = new Database($pdo);
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode("/", $_SERVER["REQUEST_URI"]);
+$input = json_decode(file_get_contents('php://input'), true);
 
 switch ($method) {
     case 'GET':
-        echo "used GET";
+        getFlashcards($pdo);
         break;
     case 'POST':
         echo "used POST";
@@ -26,4 +23,11 @@ switch ($method) {
         header("HTTP/1.0 405 Method Not Allowed");
         break;
 }
-?>
+
+function getFlashcards($pdo)
+{
+    $stmt = $pdo->prepare("SELECT * FROM flashcards");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($result);
+}
